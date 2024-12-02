8aa449ca6277341cc8d4ae8208f132049833d236
wwwxsv19 <wwwxsv19@gmail.com>
2024-12-02 09:44:02 +0900

    set : project

---
+++ README.md
@@ -0,0 +1,9 @@
+# OS Project 'MEMO' Server
+
+### 실행 방법
+
+```shell
+> git clone https://github.com/wwwxsv19/OS_Project_Server.git
+> ./gradlew build # gradle 설치
+> ./gradlew bootRun # gradle 실행
+```
---
+++ src/test/java/mine/osproject/OsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mine.osproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
